e204f0888e5edfb24a34c8bef251ebade2dbb56d
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-06 12:59:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,33 +4,21 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  extend Findable, Memorable::ClassMethods
+  include Memorable::InstanceMethods, Paramable
+
+  @@all = []
 
   def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    self.clas.all << self
   end
 
   def self.all
-    @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
+    @@all
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
